72cdf169c94100189cddf8e6a3df6cd071fd8d9a
Талалаев Алексей <dealershowers@AL.MAIN>
2022-07-21 20:13:27 +0300

    terraform_v1.1

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,3 @@
-terraform {
-  required_providers {
-    yandex = {
-      source = "yandex-cloud/yandex"
-    }
-  }
-  required_version = ">= 0.13"
-}
 provider "yandex" {
   token     = var.token_id
   cloud_id  = var.cloud_id
